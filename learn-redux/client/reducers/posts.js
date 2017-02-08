2ca4f26b1ff178149ce6d0ad3c9832ba4a1193b9
Seton Brown <setonb@gmail.com>
2017-02-07 22:13:07 -0500

    Lesson 9 - manually calling dispatch

--- learn-redux/client/reducers/posts.js
+++ learn-redux/client/reducers/posts.js
@@ -2,6 +2,7 @@
 // 1) The Action (info about what happened)
 // 2) Current State
 function posts(state=[], action) {
+  console.log('The post will change');
   console.log(state, action);
   return state;
 }
