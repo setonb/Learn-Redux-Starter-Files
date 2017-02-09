6e2205cdc9247805b30b837d311d12d03d351ea6
Seton Brown <setonb@gmail.com>
2017-02-09 17:10:33 -0500

    Lesson 12 - Adding onClick to pass increment_like action through the reducers. Remember functions need to be pure.

--- learn-redux/client/reducers/posts.js
+++ learn-redux/client/reducers/posts.js
@@ -2,9 +2,17 @@
 // 1) The Action (info about what happened)
 // 2) Current State
 function posts(state=[], action) {
-  console.log('The post will change');
-  console.log(state, action);
-  return state;
+  switch (action.type) {
+    case 'INCREMENT_LIKES':
+      const i = action.index;
+      return [
+        ...state.slice(0,i), // before the item we want
+        {...state[i], likes: state[i].likes + 1},
+        ...state.slice(i + 1) // after the item we want
+      ]
+    default:
+      return state;
+  }
 }
 
 export default posts;
