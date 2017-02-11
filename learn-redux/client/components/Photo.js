efd02451134ff94c02f66bcb43f76a3513e4d70c
Seton Brown <setonb@gmail.com>
2017-02-10 22:44:23 -0500

    Adding space before comment number

--- learn-redux/client/components/Photo.js
+++ learn-redux/client/components/Photo.js
@@ -28,7 +28,7 @@ const Photo = React.createClass({
             <Link className="button" to={`/view/${post.code}`}>
               <span className="comment-count">
                 <span className="speech-bubble"></span>
-                {comments[post.code] ? comments[post.code].length : ' 0'}
+                {comments[post.code] ? ' ' + comments[post.code].length : ' 0'}
               </span>
             </Link>
           </div>
